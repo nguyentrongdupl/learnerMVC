f1dc5da6ba51acd9dffc2cce1a1a2d460f773f73
Joe Nguyen <nguyentrongdupl@gmail.com>
2024-12-13 21:26:23 +0700

    Fix error file

--- Hemis-master/Hemis-master/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace C500Hemis.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Hemis-master/Hemis-master/Models/VKhoiNganhDaoTao.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace C500Hemis.Models;
-
-public partial class VKhoiNganhDaoTao
-{
-    public string? KhoiNganhDaoTao { get; set; }
-}
